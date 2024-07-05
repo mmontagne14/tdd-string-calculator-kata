9bec6b566521351324b25d650a2b3ec023738889
mmontagne <marcelomontagne@gmail.com>
2024-07-05 11:05:42 -0300

    Try1

--- src/StringCalculatorKata.Test/StringCalculatorTest.cs
+++ src/StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -1,9 +1,70 @@
+using System;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using StringCalculatorKata;
 
 namespace StringCalculatorKata.Test
 {
     public class StringCalculatorTest
     {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(""));
+        }
+
+        [TestCase("5", 5)]
+        [TestCase("7", 7)]
+        public void Add_SingleNumber_ReturnsThatNumber(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("2,3", 5)]
+        [TestCase("3,5", 8)]
+        public void Add_TwoNumbers_ReturnSumThemUp(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("2,3,3", 8)]
+        [TestCase("10,25,3", 38)]
+        [TestCase("10,25,3,1,1", 40)]
+        public void Add_MultipleNumbers_ReturnsSumThemUp(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("1\n2,3", 6)]
+        [TestCase("2\n4,8", 14)]
+        [TestCase("2\n4,8\n10", 24)]
+        public void Add_MultipleNumbers_NewLineAsDelimiter(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//.\n5.3", 8)]
+        public void Add_MultipleNumbers_WithDelimiterSetting(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("-1", -1)]
+        [TestCase("-15", -15)]
+        [TestCase("2\n4,-8\n10", -8)]
+        public void Add_MultipleNumbers_OneNegativeNumberRestriction(string numbers, int expected)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => { StringCalculator.Add(numbers); });
+            StringAssert.Contains(String.Format("negatives not allowed : {0}", expected.ToString()), ex.Message.ToString());
+        }
+
+        [TestCase("2\n4,-7\n-10", "-7 -10")]
+        [TestCase("-2\n-4,-7\n-10", "-2 -4 -7 -10")]
+        public void Add_MultipleNumbers_MultipleNegativeNumbersRestriction(string numbers, string expected)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => { StringCalculator.Add(numbers); });
+            StringAssert.Contains(String.Format("negatives not allowed : {0}", expected), ex.Message.ToString());
+        }
     }
 }
--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
 
 namespace StringCalculatorKata
 {
     public class StringCalculator
-    {        
+    {
+        public static int Add(string numbers)
+        {
+            if (numbers.Length == 0)
+                return 0;
+
+            char delimiter = ',';
+            if (numbers.Contains("//"))
+            {
+                numbers = ExtractDelimiter(numbers, out delimiter);
+            };
+            var numbersArray = SplitByDelimiter(numbers, delimiter);
+
+            DetectNegativeNumbers(numbersArray);
+
+            return numbers.Replace('\n', delimiter).Split(delimiter).Select(x => int.Parse(x)).Sum();
+        }
+
+        private static string[] SplitByDelimiter(string numbers, char delimiter)
+        {
+            return numbers.Replace('\n', delimiter).Split(delimiter);
+        }
+
+        private static string ExtractDelimiter(string numbers, out char defaultDelimiter)
+        {
+            defaultDelimiter = numbers.ToCharArray()[2];
+            return numbers[4..];
+        }
+
+        private static void DetectNegativeNumbers(string[] numbersArray)
+        {
+            if (numbersArray.Any(x => int.Parse(x) < 0))
+            {
+                var negativeNumberList = numbersArray.Where(x => int.Parse(x) < 0).ToList();
+                var sb = new System.Text.StringBuilder();
+                negativeNumberList.ForEach(x =>
+                {
+                    string negativeNumberToAdd = String.Format(int.Parse(x) + " ");
+                    sb.Append(negativeNumberToAdd);
+                });
+                throw new ArgumentException(String.Format("negatives not allowed : {0}", sb.ToString()));
+            }
+        }
     }
 }
