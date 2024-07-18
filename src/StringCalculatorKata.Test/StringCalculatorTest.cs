2a1df0144dbfb667256c2cab078d8321d167a9dd
mmontagne <marcelomontagne@gmail.com>
2024-07-18 16:30:05 -0300

    Commit

--- src/StringCalculatorKata.Test/StringCalculatorTest.cs
+++ src/StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography;
 using NUnit.Framework;
 using StringCalculatorKata;
 
@@ -5,5 +7,43 @@ namespace StringCalculatorKata.Test
 {
     public class StringCalculatorTest
     {
+        [Test]
+        public void Add_StringEmpty_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(""));
+        }
+
+        [TestCase(5, "5")]
+        [TestCase(1, "1")]
+        public void Add_OneNumber_ReturnsSameNumber(int expected, string numbers)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase(3, "1,2")]
+        [TestCase(5, "3,2")]
+        public void Add_TwoNumbers_ReturnsSumThemUp(int expected, string numbers)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase(10, "3,5,2")]
+        [TestCase(20, "3,5,2,10")]
+        public void Add_UnknownAmountOfNumbers(int expected, string numbers)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+        [TestCase(6, "1\n2, 3")]
+        [TestCase(10, "1\n2,3\n4")]
+        public void Add_NewLinesBetweenNumbers(int expected, string numbers)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [Test]
+        public void Add_SupportDifferentDelimiters()
+        {
+            Assert.AreEqual(3, StringCalculator.Add("//;\n1;2"));
+        }
     }
 }
--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StringCalculatorKata
 {
     public class StringCalculator
-    {        
+    {
+        public static int Add(string numbers)
+        {
+            int res = 0;
+            if (string.IsNullOrEmpty(numbers))
+                return res;
+
+            if (int.TryParse(numbers, out res))
+                return res;
+
+            foreach (int number in ExtractIntArray(numbers))
+            {
+                res += number;
+            }
+            return res;
+        }
+
+        public static List<int> ExtractIntArray(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                char delimiter = numbers[3];
+                int lineIndex = numbers.IndexOf("\n");
+                return numbers.Substring(lineIndex + 1).Replace('\n', delimiter).Split(delimiter).Select(x => int.Parse(x)).ToList();
+            }
+
+            return numbers.Replace("\n", ",").Split(",").Select(x => int.Parse(x)).ToList();
+        }
     }
 }
